6e36b72b8e1f2ee9e32c6b00a12c15a77f2a81e6
ycraah <ycraah@gmail.com>
2024-08-06 20:00:51 +0900

    "연습문제 1번 서블릿에 로그인 요청 시 유효성 검사하기 정답...아니 애초에 이렇게 만드라고 이야기를 해주던가"

--- src/main/java/ycraah/web/web/servletPrc/Ex1_1.java
+++ src/main/java/ycraah/web/web/servletPrc/Ex1_1.java
@@ -20,10 +20,19 @@ public class Ex1_1 extends HttpServlet {
     String user_id = request.getParameter("user_id");
     String user_pw = request.getParameter("user_pw");
 
-    out.print("<html><head><title>로그인 내용 확인창</title></head><body>");
-    out.printf("<p style=\"font-size:20px\">입력된 아이디는 %s입니다.</p>", user_id);
-    out.printf("<p style=\"font-size:30px\">입력된 비밀번호는 %s입니다.</p>", user_pw);
-    out.print("</body></html>");
-    out.close();
+    System.out.println("아이디 : " + user_id);
+    System.out.println("비밀번호 : " + user_pw);
+
+    if((user_id != null && user_id.length() != 0) && (user_pw != null && user_pw.length() != 0)){
+      out.print("<html><head><title>로그인 성공</title></head><body>");
+      out.printf("<p>%s님의 로그인을 환영합니다.</p>", user_id);
+      out.print("</body></html>");
+    } else {
+      out.print("<html><head><title>로그인 실패</title></head><body>");
+      out.print("<p>아이디를 입력하세요!!</p><br>");
+      out.print("<a href='http://localhost:8080/loginTest.html'>로그인 창으로 이동</a>");
+      out.print("</body></html>");
+    }
+
   }
 }
