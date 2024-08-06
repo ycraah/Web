64cbc7e63a002be5211f8f7998493ab2117e4b71
ycraah <ycraah@gmail.com>
2024-08-06 18:23:14 +0900

    "로그인 정보를 doPost로 전달"

---
+++ src/main/java/ycraah/web/web/login/LoginServlet3.java
@@ -0,0 +1,32 @@
+package ycraah.web.web.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login3")
+public class LoginServlet3 extends HttpServlet {
+  @Override
+  public void init() throws ServletException {
+    System.out.println("init 메서드 호출");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    request.setCharacterEncoding("utf-8");
+    String user_id = request.getParameter("user_id");
+    String user_pw = request.getParameter("user_pw");
+    System.out.println("아이디: " + user_id);
+    System.out.println("패스워드: " + user_pw);
+
+  }
+
+  @Override
+  public void destroy() {
+    System.out.println("destroy 메서드 호출");
+  }
+}
